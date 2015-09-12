0d7462b2d3f6f4281350dedeb13525cf63771f27
santtone <sami.anttonen@fifthelement.fi>
2015-09-12 23:45:19 +0300

    Created WWW project structure

---
+++ www/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name geolocationTrackerApp
+ * @description
+ * # geolocationTrackerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('geolocationTrackerApp', []);
---
+++ www/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geolocationTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the geolocationTrackerApp
+ */
+angular.module('geolocationTrackerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
